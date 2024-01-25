64b1f662072350c3d3ae7cdd2744178ae06f6306
Öznur Demir <dmroznurr@gmail.com>
2024-01-25 11:06:47 +0300

    Completed Home Fragment UI

--- app/src/main/java/com/example/babytracker/ui/fragments/premium/PremiumFragment.kt
+++ app/src/main/java/com/example/babytracker/ui/fragments/premium/PremiumFragment.kt
@@ -5,12 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.babytracker.R
 import com.example.babytracker.databinding.FragmentPremiumBinding
 
 class PremiumFragment : Fragment() {
     private lateinit var binding: FragmentPremiumBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentPremiumBinding.inflate(inflater, container, false)
+        binding.apply {
+            buttonPremium.setOnClickListener {
+                findNavController().navigate(R.id.action_premiumFragment_to_homeFragment)
+            }
+        }
         return binding.root
     }
 }
