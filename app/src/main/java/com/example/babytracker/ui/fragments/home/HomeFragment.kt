248e0c03be71a9c588ab0422a899b778132a33a1
Öznur Demir <dmroznurr@gmail.com>
2024-01-25 11:34:18 +0300

    Completed Home Fragment Logic

--- app/src/main/java/com/example/babytracker/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/babytracker/ui/fragments/home/HomeFragment.kt
@@ -5,12 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.babytracker.R
 import com.example.babytracker.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding.apply {
+            imageViewSettings.setOnClickListener {
+                findNavController().navigate(R.id.action_homeFragment_to_settingsFragment)
+            }
+            imageViewCalendar.setOnClickListener {
+                findNavController().navigate(R.id.action_homeFragment_to_calenderFragment)
+            }
+            buttonFeeding.setOnClickListener {
+                findNavController().navigate(R.id.action_homeFragment_to_feedingFragment)
+            }
+            buttonSleep.setOnClickListener {
+                findNavController().navigate(R.id.action_homeFragment_to_sleepFragment)
+            }
+            buttonSymptoms.setOnClickListener {
+                findNavController().navigate(R.id.action_homeFragment_to_symptomsFragment)
+            }
+        }
         return binding.root
     }
 }
